dee5bc8589d8a484794e481bdc28f3fa5f137f46
master <armaev98@mail.ru>
2026-05-26 12:01:18 +0800

    Добавлена логическая коррекция

---
+++ WTQ/Qwen/logic_pandas_code.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from send_message import send_message_async, ModelMessageDict
+import asyncio
+from config import logic_prompt
+from utils.type_check import analyze_dataset_parallel
+
+
+async def get_pandas(question: str, ser_tbl: str, tbl_types: dict,pandas_code:str, error:str, temperature: float,max_rows=20):
+
+    system_message = ModelMessageDict(role = 'system')
+    system_message.add_text_content(logic_prompt)
+
+    user_message = ModelMessageDict(role='user')
+    user_message.add_text_content(
+        f"QUESTION: {question}\n"
+        f"AVAILABLE COLUMNS: {', '.join(list(tbl_types.keys()))}\n"
+        #f"COLUMN TYPES:\n{tbl_types}\n"
+        f"TABLE DATA:\n{ser_tbl}"
+        f"PANDAS CODE: \n{pandas_code}"
+    )
+
+    success, responses = await send_message_async(
+        messages=[system_message, user_message
+        ],
+        base_url="http://127.0.0.1:9123/v1",
+        api_key='EMPTY',
+        model_name='Qwen/Qwen3-Coder-Next',
+        temperature=temperature,
+    )
+
+    if not success:
+        return None, f"LLM error: {responses}"
+
+    return responses[0]
